ba792de6ce4d0543222558467c8d9675885655fe
James Mack <wolfbro@gmail.com>
2012-09-04 11:22:17 -0700

    Schema updates, new script to generate code s cells.

--- gsl-schema.sql
+++ gsl-schema.sql
@@ -1,20 +1,21 @@
 CREATE TABLE season (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO INCREMENT,
     name VARCHAR(50),
     year YEAR,
     season INT
 )
 
 CREATE TABLE status (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO INCREMENT,
     name VARCHAR(50),
+    value VARCHAR(10),
     code CHAR(1),
     season INT REFERENCES season.id,
     user INT REFERENCES user.id
 )
 
 CREATE TABLE user (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO INCREMENT,
     name VARCHAR(50)
 )
 
